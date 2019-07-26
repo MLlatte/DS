0cff3a5a9ba5e21b7fe82d3d9912a6dc4aada01f
MLlatte <1455787505@qq.com>
2019-07-26 13:54:56 +0800

    图的深度优先搜索
    图的深度优先搜索

--- 图/图的遍历/图的深度优先搜索.cpp
+++ 图/图的遍历/图的深度优先搜索.cpp
@@ -5,19 +5,19 @@ using namespace std;
 # define maxSize 100
 typedef struct ArcNode
 {
-	int adjvex;       //领接顶点(vertex（vex）是顶点的意思)
+	int adjvex;                //领接顶点(vertex（vex）是顶点的意思)
 	struct ArcNode *nextarc;  //指向下一条边的指针
-	int info;          //边对应的权值
+	int info;                //边对应的权值
 }ArcNode;
 typedef struct
 {
-	char data;           //顶点信息
-	ArcNode *firstarc;  //指向第一条边的指针
-}VNode;                  //(arc是"弧"的意思，有向图中,边称为弧)
+	char data;            //顶点信息
+	ArcNode *firstarc;   //指向第一条边的指针
+}VNode;                     //(arc是"弧"的意思，有向图中,边称为弧)
 typedef struct
 {
-	VNode adjlist[maxSize];//图的顶点数组(邻接表)
-	int n, e;  //顶点数和边数
+	VNode adjlist[maxSize];    //图的顶点数组(邻接表)
+	int n, e;                 //顶点数和边数
 }AGraph;
 AGraph *g;
 
@@ -25,7 +25,7 @@ void creatAGraph(AGraph *&g)
 {
 	g = (AGraph*)malloc(sizeof(AGraph));
 	int i, j, k;
-	ArcNode *p;     //p指向待插入的结点
+	ArcNode *p;       //p指向待插入的结点
 
 	//输入顶点和边数
 	cout << "输入顶点数和边数" << endl;
@@ -36,7 +36,7 @@ void creatAGraph(AGraph *&g)
 	for (i = 0; i < g->n; i++)
 	{
 		cin >> g->adjlist[i].data;
-		g->adjlist[i].firstarc = NULL; //将指向边表的指针初始化(置空)
+		g->adjlist[i].firstarc = NULL;   //将指向边表的指针初始化(置空)
 	}
 
 	//建立边表
@@ -46,13 +46,13 @@ void creatAGraph(AGraph *&g)
 		cin >> i >> j;
 
 		//单链表中的头插法
-		p = (ArcNode*)malloc(sizeof(ArcNode));//向内存申请空间，生成边表结点
+		p = (ArcNode*)malloc(sizeof(ArcNode));   //向内存申请空间，生成边表结点
 
 		p->adjvex = j;  //存储弧头
 
 		//头插法建立边链表
-		p->nextarc = g->adjlist[i].firstarc;  // 将p的指针指向当前顶点指向的结点
-		g->adjlist[i].firstarc = p;//将当前顶点的指针指向p
+		p->nextarc = g->adjlist[i].firstarc;   // 将p的指针指向当前顶点指向的结点
+		g->adjlist[i].firstarc = p;           //将当前顶点的指针指向p
 
 		//下面代码有向图无,无向图有 
 		p = (ArcNode*)malloc(sizeof(ArcNode));
@@ -62,19 +62,19 @@ void creatAGraph(AGraph *&g)
 	}
 }
 
-int visit[maxSize]; //全局数组，作为顶点的访问标记，初始时所有元素为0，表示所有顶点都未被访问
+int visit[maxSize];    //全局数组，作为顶点的访问标记，初始时所有元素为0，表示所有顶点都未被访问
 
-void DFS(AGraph *G, int v)  //v不是顶点，v是某个顶点所在顶点数组中的下标
+void DFS(AGraph *G, int v)    //v不是顶点，v是某个顶点所在顶点数组中的下标
 {
-	ArcNode *p;       //指向ArcNode型变量的指针
-	visit[v] = 1;    //置已访问标记
-	cout << g->adjlist[v].data << endl;  //访问顶点
-	p = G->adjlist[v].firstarc;   //p指向顶点v的第一条边(即通过v找到其对应的顶点，进而找到顶点所引出的第一条边)
+	ArcNode *p;         //指向ArcNode型变量的指针
+	visit[v] = 1;      //置已访问标记
+	cout << g->adjlist[v].data << endl;   //访问顶点
+	p = G->adjlist[v].firstarc;          //p指向顶点v的第一条边(即通过v找到其对应的顶点，进而找到顶点所引出的第一条边)
     while (p != NULL)
     {
-		if (visit[p->adjvex] == 0)//若顶点未访问，则递归访问它
+		if (visit[p->adjvex] == 0)    //若顶点未访问，则递归访问它
 			DFS(G, p->adjvex);
-		p = p->nextarc;   //p指向顶点v的下一条边的终点(原路返回，看还有没有其他未访问的顶点)
+		p = p->nextarc;              //p指向顶点v的下一条边的终点(原路返回，看还有没有其他未访问的顶点)
     }
 }
 int main() {
