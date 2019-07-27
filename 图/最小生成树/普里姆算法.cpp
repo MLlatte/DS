cc432a33457c43452b5e587ebf420ca10fd3a691
MLlatte <1455787505@qq.com>
2019-07-27 10:28:54 +0800

    prim算法
    prim算法

--- 图/最小生成树/普里姆算法.cpp
+++ 图/最小生成树/普里姆算法.cpp
@@ -5,41 +5,41 @@ using namespace std;
 #define INF 9999 
 typedef struct
 {
-	int edges[maxSize][maxSize];//邻接矩阵定义
-	int n, e;          //顶点数和边数
-	char vex[maxSize];//存放结点信息
-}MGraph;         //图的邻接矩阵类型
+	int edges[maxSize][maxSize];      //邻接矩阵定义
+	int n, e;                         //顶点数和边数
+	char vex[maxSize];               //存放结点信息
+}MGraph;                                //图的邻接矩阵类型
 
 //用图的邻接矩阵存储，因为目的是构造MST，所以存储结构越简单越好
-void prim(MGraph g, int v0, int &sum)//v0是构造最小生成树的起始顶点
+void prim(MGraph g, int v0, int &sum)         //v0是构造最小生成树的起始顶点
 {
 	
-	int lowcost[maxSize], vset[maxSize], v; //v表示当前刚并入的那个顶点
-	int i, j, k = 0, min; //用min选出与当前生成树相连的并且到图中剩余顶点之间的最小的边
+	int lowcost[maxSize], vset[maxSize], v;       //v表示当前刚并入的那个顶点
+	int i, j, k = 0, min;                        //用min选出与当前生成树相连的并且到图中剩余顶点之间的最小的边
 	v = v0;  
 	fill(vset, vset + maxSize, 0);
 	for (int i = 0; i < g.n; ++i)
 	{
-		lowcost[i] = g.edges[v0][i];//初值是v0到其余顶点的边的最小权值
+		lowcost[i] = g.edges[v0][i];     //初值是v0到其余顶点的边的最小权值
 		vset[i] = 0;
 	}
-	vset[v0] = 1;   //将v0并入树中
-	sum = 0;     //将sum清零用来累计树的权值
+	vset[v0] = 1;     //将v0并入树中
+	sum = 0;         //将sum清零用来累计树的权值
 	for (i = 0; i < g.n-1; ++i)
 	{
-		min = INF; //INF是一个已经定义的比图中所有边权值都大的常量
-		            //在这里某顶点到其自身的距离规定为无穷大，即INF
+		min = INF;     //INF是一个已经定义的比图中所有边权值都大的常量
+		              //在这里某顶点到其自身的距离规定为无穷大，即INF
 		
 		/*下面这个循环用于选出候选边中最小者*/
 		for (j = 0; j < g.n; j++)
-			if (vset[j] == 0 && lowcost[j] < min)//选出当前生成树到
-			{                 //其余顶点最短边中的最短的一条
+			if (vset[j] == 0 && lowcost[j] < min)    //选出当前生成树到
+			{                                       //其余顶点最短边中的最短的一条
 				min = lowcost[j];
-				k = j;  //这里一定要把j赋给k,才能保留最短边对应的那个顶点
+				k = j;                         //这里一定要把j赋给k,才能保留最短边对应的那个顶点
 			}
 		vset[k] = 1;
 		v = k;
-		sum += min;  //这里用sum记录了最小生成树的权值
+		sum += min;       //这里用sum记录了最小生成树的权值
 		
 		/*下面这个循环以刚进入的顶点v为媒介更新候选边*/
 //考查所有剩余顶点vj,如果(v,vj)的权值比lowcost[vj]小，则用
@@ -76,7 +76,7 @@ int main()
 	}
 
 	prim(g, 0, sum);
-	cout << sum << endl;  //输出最小生成树的权值
+	cout << sum << endl;          //输出最小生成树的权值
 
 	//打印邻接矩阵
 	for (int i = 0; i < g.n; ++i) {
